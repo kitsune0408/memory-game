6c71ea777a47767e1eb88358586a6eed5f7d6a58
kitsune0408 <kitsune0408@yahoo.com>
2015-05-15 18:33:08 +0300

    memory test game

---
+++ MemoryGame/Form1.Designer.cs
@@ -0,0 +1,387 @@
+﻿namespace MemoryGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.b01 = new System.Windows.Forms.Button();
+            this.b02 = new System.Windows.Forms.Button();
+            this.b00 = new System.Windows.Forms.Button();
+            this.b03 = new System.Windows.Forms.Button();
+            this.b04 = new System.Windows.Forms.Button();
+            this.b05 = new System.Windows.Forms.Button();
+            this.b15 = new System.Windows.Forms.Button();
+            this.b14 = new System.Windows.Forms.Button();
+            this.b13 = new System.Windows.Forms.Button();
+            this.b10 = new System.Windows.Forms.Button();
+            this.b12 = new System.Windows.Forms.Button();
+            this.b11 = new System.Windows.Forms.Button();
+            this.b25 = new System.Windows.Forms.Button();
+            this.b24 = new System.Windows.Forms.Button();
+            this.b23 = new System.Windows.Forms.Button();
+            this.b20 = new System.Windows.Forms.Button();
+            this.b22 = new System.Windows.Forms.Button();
+            this.b21 = new System.Windows.Forms.Button();
+            this.b35 = new System.Windows.Forms.Button();
+            this.b34 = new System.Windows.Forms.Button();
+            this.b33 = new System.Windows.Forms.Button();
+            this.b30 = new System.Windows.Forms.Button();
+            this.b32 = new System.Windows.Forms.Button();
+            this.b31 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.triesLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // b01
+            // 
+            this.b01.Location = new System.Drawing.Point(78, 55);
+            this.b01.Name = "b01";
+            this.b01.Size = new System.Drawing.Size(60, 60);
+            this.b01.TabIndex = 0;
+            this.b01.UseVisualStyleBackColor = true;
+            this.b01.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // b02
+            // 
+            this.b02.Location = new System.Drawing.Point(144, 55);
+            this.b02.Name = "b02";
+            this.b02.Size = new System.Drawing.Size(60, 60);
+            this.b02.TabIndex = 1;
+            this.b02.UseVisualStyleBackColor = true;
+            this.b02.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // b00
+            // 
+            this.b00.Location = new System.Drawing.Point(12, 55);
+            this.b00.Name = "b00";
+            this.b00.Size = new System.Drawing.Size(60, 60);
+            this.b00.TabIndex = 2;
+            this.b00.UseVisualStyleBackColor = true;
+            this.b00.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // b03
+            // 
+            this.b03.Location = new System.Drawing.Point(210, 55);
+            this.b03.Name = "b03";
+            this.b03.Size = new System.Drawing.Size(60, 60);
+            this.b03.TabIndex = 3;
+            this.b03.UseVisualStyleBackColor = true;
+            this.b03.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // b04
+            // 
+            this.b04.Location = new System.Drawing.Point(276, 55);
+            this.b04.Name = "b04";
+            this.b04.Size = new System.Drawing.Size(60, 60);
+            this.b04.TabIndex = 4;
+            this.b04.UseVisualStyleBackColor = true;
+            this.b04.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // b05
+            // 
+            this.b05.Location = new System.Drawing.Point(342, 55);
+            this.b05.Name = "b05";
+            this.b05.Size = new System.Drawing.Size(60, 60);
+            this.b05.TabIndex = 5;
+            this.b05.UseVisualStyleBackColor = true;
+            this.b05.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // b15
+            // 
+            this.b15.Location = new System.Drawing.Point(342, 121);
+            this.b15.Name = "b15";
+            this.b15.Size = new System.Drawing.Size(60, 60);
+            this.b15.TabIndex = 11;
+            this.b15.UseVisualStyleBackColor = true;
+            this.b15.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // b14
+            // 
+            this.b14.Location = new System.Drawing.Point(276, 121);
+            this.b14.Name = "b14";
+            this.b14.Size = new System.Drawing.Size(60, 60);
+            this.b14.TabIndex = 10;
+            this.b14.UseVisualStyleBackColor = true;
+            this.b14.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // b13
+            // 
+            this.b13.Location = new System.Drawing.Point(210, 121);
+            this.b13.Name = "b13";
+            this.b13.Size = new System.Drawing.Size(60, 60);
+            this.b13.TabIndex = 9;
+            this.b13.UseVisualStyleBackColor = true;
+            this.b13.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // b10
+            // 
+            this.b10.Location = new System.Drawing.Point(12, 121);
+            this.b10.Name = "b10";
+            this.b10.Size = new System.Drawing.Size(60, 60);
+            this.b10.TabIndex = 8;
+            this.b10.UseVisualStyleBackColor = true;
+            this.b10.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // b12
+            // 
+            this.b12.Location = new System.Drawing.Point(144, 121);
+            this.b12.Name = "b12";
+            this.b12.Size = new System.Drawing.Size(60, 60);
+            this.b12.TabIndex = 7;
+            this.b12.UseVisualStyleBackColor = true;
+            this.b12.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // b11
+            // 
+            this.b11.Location = new System.Drawing.Point(78, 121);
+            this.b11.Name = "b11";
+            this.b11.Size = new System.Drawing.Size(60, 60);
+            this.b11.TabIndex = 6;
+            this.b11.UseVisualStyleBackColor = true;
+            this.b11.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // b25
+            // 
+            this.b25.Location = new System.Drawing.Point(342, 187);
+            this.b25.Name = "b25";
+            this.b25.Size = new System.Drawing.Size(60, 60);
+            this.b25.TabIndex = 17;
+            this.b25.UseVisualStyleBackColor = true;
+            this.b25.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // b24
+            // 
+            this.b24.Location = new System.Drawing.Point(276, 187);
+            this.b24.Name = "b24";
+            this.b24.Size = new System.Drawing.Size(60, 60);
+            this.b24.TabIndex = 16;
+            this.b24.UseVisualStyleBackColor = true;
+            this.b24.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // b23
+            // 
+            this.b23.Location = new System.Drawing.Point(210, 187);
+            this.b23.Name = "b23";
+            this.b23.Size = new System.Drawing.Size(60, 60);
+            this.b23.TabIndex = 15;
+            this.b23.UseVisualStyleBackColor = true;
+            this.b23.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // b20
+            // 
+            this.b20.Location = new System.Drawing.Point(12, 187);
+            this.b20.Name = "b20";
+            this.b20.Size = new System.Drawing.Size(60, 60);
+            this.b20.TabIndex = 14;
+            this.b20.UseVisualStyleBackColor = true;
+            this.b20.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // b22
+            // 
+            this.b22.Location = new System.Drawing.Point(144, 187);
+            this.b22.Name = "b22";
+            this.b22.Size = new System.Drawing.Size(60, 60);
+            this.b22.TabIndex = 13;
+            this.b22.UseVisualStyleBackColor = true;
+            this.b22.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // b21
+            // 
+            this.b21.Location = new System.Drawing.Point(78, 187);
+            this.b21.Name = "b21";
+            this.b21.Size = new System.Drawing.Size(60, 60);
+            this.b21.TabIndex = 12;
+            this.b21.UseVisualStyleBackColor = true;
+            this.b21.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // b35
+            // 
+            this.b35.Location = new System.Drawing.Point(342, 253);
+            this.b35.Name = "b35";
+            this.b35.Size = new System.Drawing.Size(60, 60);
+            this.b35.TabIndex = 23;
+            this.b35.UseVisualStyleBackColor = true;
+            this.b35.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // b34
+            // 
+            this.b34.Location = new System.Drawing.Point(276, 253);
+            this.b34.Name = "b34";
+            this.b34.Size = new System.Drawing.Size(60, 60);
+            this.b34.TabIndex = 22;
+            this.b34.UseVisualStyleBackColor = true;
+            this.b34.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // b33
+            // 
+            this.b33.Location = new System.Drawing.Point(210, 253);
+            this.b33.Name = "b33";
+            this.b33.Size = new System.Drawing.Size(60, 60);
+            this.b33.TabIndex = 21;
+            this.b33.UseVisualStyleBackColor = true;
+            this.b33.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // b30
+            // 
+            this.b30.Location = new System.Drawing.Point(12, 253);
+            this.b30.Name = "b30";
+            this.b30.Size = new System.Drawing.Size(60, 60);
+            this.b30.TabIndex = 20;
+            this.b30.UseVisualStyleBackColor = true;
+            this.b30.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // b32
+            // 
+            this.b32.Location = new System.Drawing.Point(144, 253);
+            this.b32.Name = "b32";
+            this.b32.Size = new System.Drawing.Size(60, 60);
+            this.b32.TabIndex = 19;
+            this.b32.UseVisualStyleBackColor = true;
+            this.b32.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // b31
+            // 
+            this.b31.Location = new System.Drawing.Point(78, 253);
+            this.b31.Name = "b31";
+            this.b31.Size = new System.Drawing.Size(60, 60);
+            this.b31.TabIndex = 18;
+            this.b31.UseVisualStyleBackColor = true;
+            this.b31.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(327, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "RESTART";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.restartGame);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Mistral", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.Location = new System.Drawing.Point(13, 332);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(0, 16);
+            this.timeLabel.TabIndex = 25;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // triesLabel
+            // 
+            this.triesLabel.AutoSize = true;
+            this.triesLabel.Font = new System.Drawing.Font("Segoe Print", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.triesLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.triesLabel.Location = new System.Drawing.Point(13, 19);
+            this.triesLabel.Name = "triesLabel";
+            this.triesLabel.Size = new System.Drawing.Size(87, 23);
+            this.triesLabel.TabIndex = 26;
+            this.triesLabel.Text = "Tries left: 3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 367);
+            this.Controls.Add(this.triesLabel);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.b35);
+            this.Controls.Add(this.b34);
+            this.Controls.Add(this.b33);
+            this.Controls.Add(this.b30);
+            this.Controls.Add(this.b32);
+            this.Controls.Add(this.b31);
+            this.Controls.Add(this.b25);
+            this.Controls.Add(this.b24);
+            this.Controls.Add(this.b23);
+            this.Controls.Add(this.b20);
+            this.Controls.Add(this.b22);
+            this.Controls.Add(this.b21);
+            this.Controls.Add(this.b15);
+            this.Controls.Add(this.b14);
+            this.Controls.Add(this.b13);
+            this.Controls.Add(this.b10);
+            this.Controls.Add(this.b12);
+            this.Controls.Add(this.b11);
+            this.Controls.Add(this.b05);
+            this.Controls.Add(this.b04);
+            this.Controls.Add(this.b03);
+            this.Controls.Add(this.b00);
+            this.Controls.Add(this.b02);
+            this.Controls.Add(this.b01);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Test your memory!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b01;
+        private System.Windows.Forms.Button b02;
+        private System.Windows.Forms.Button b00;
+        private System.Windows.Forms.Button b03;
+        private System.Windows.Forms.Button b04;
+        private System.Windows.Forms.Button b05;
+        private System.Windows.Forms.Button b15;
+        private System.Windows.Forms.Button b14;
+        private System.Windows.Forms.Button b13;
+        private System.Windows.Forms.Button b10;
+        private System.Windows.Forms.Button b12;
+        private System.Windows.Forms.Button b11;
+        private System.Windows.Forms.Button b25;
+        private System.Windows.Forms.Button b24;
+        private System.Windows.Forms.Button b23;
+        private System.Windows.Forms.Button b20;
+        private System.Windows.Forms.Button b22;
+        private System.Windows.Forms.Button b21;
+        private System.Windows.Forms.Button b35;
+        private System.Windows.Forms.Button b34;
+        private System.Windows.Forms.Button b33;
+        private System.Windows.Forms.Button b30;
+        private System.Windows.Forms.Button b32;
+        private System.Windows.Forms.Button b31;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label triesLabel;
+    }
+}
+
---
+++ MemoryGame/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using System.IO;
+
+namespace MemoryGame
+{
+    public partial class Form1 : Form
+    {
+        //matrix for storing images for each button
+        string[,] matrix = new string[4, 6];
+        //array with images
+        string[] images = new string[] {
+            Path.Combine(Environment.CurrentDirectory, @"Images\p001.png"),
+            Path.Combine(Environment.CurrentDirectory, @"Images\p002.png"),
+            Path.Combine(Environment.CurrentDirectory, @"Images\p003.png"),
+            Path.Combine(Environment.CurrentDirectory, @"Images\p004.png"),
+            Path.Combine(Environment.CurrentDirectory, @"Images\p005.png"),
+            Path.Combine(Environment.CurrentDirectory, @"Images\p006.png"),
+            Path.Combine(Environment.CurrentDirectory, @"Images\p007.png"),
+            Path.Combine(Environment.CurrentDirectory, @"Images\p008.png"),
+            Path.Combine(Environment.CurrentDirectory, @"Images\p009.png"),
+            Path.Combine(Environment.CurrentDirectory, @"Images\p010.png"),
+            Path.Combine(Environment.CurrentDirectory, @"Images\p011.png"),
+            Path.Combine(Environment.CurrentDirectory, @"Images\p012.png"),
+            Path.Combine(Environment.CurrentDirectory, @"Images\p001.png"),
+            Path.Combine(Environment.CurrentDirectory, @"Images\p002.png"),
+            Path.Combine(Environment.CurrentDirectory, @"Images\p003.png"),
+            Path.Combine(Environment.CurrentDirectory, @"Images\p004.png"),
+            Path.Combine(Environment.CurrentDirectory, @"Images\p005.png"),
+            Path.Combine(Environment.CurrentDirectory, @"Images\p006.png"),
+            Path.Combine(Environment.CurrentDirectory, @"Images\p007.png"),
+            Path.Combine(Environment.CurrentDirectory, @"Images\p008.png"),
+            Path.Combine(Environment.CurrentDirectory, @"Images\p009.png"),
+            Path.Combine(Environment.CurrentDirectory, @"Images\p010.png"),
+            Path.Combine(Environment.CurrentDirectory, @"Images\p011.png"),
+            Path.Combine(Environment.CurrentDirectory, @"Images\p012.png")
+        };
+        //counter in seconds - 10 seconds
+        int counter = 10;
+        int triesLeft = 2; //number of tries left
+        bool secondPress = false;//check if it's the second button which is pressed
+        //images for comparison
+        string prevImage;
+        string curImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            showImages();
+        }
+
+        //press button to reveal image
+        private void buttonClick(object sender, EventArgs e)
+        {
+
+            if (triesLeft != 0)
+            {
+                Button curButton = sender as Button;
+                int b1 = int.Parse(curButton.Name.Substring(1, 1));
+                int b2 = int.Parse(curButton.Name.Substring(2, 1));
+
+                if (secondPress)
+                {
+                    curImage = matrix[b1, b2];
+                    if (prevImage == curImage)
+                    {
+                        curButton.BackgroundImage = Image.FromFile(matrix[b1, b2]);
+                        curButton.Enabled = false;
+                        secondPress = false;
+                    }
+                    else
+                    {
+                        triesLabel.Text = "Tries left: " + triesLeft;
+                        triesLeft -= 1;
+                        curButton.BackgroundImage = null;
+                    }
+                }
+                else
+                {
+                    secondPress = true;
+                    prevImage = matrix[b1, b2];
+                    curButton.BackgroundImage = Image.FromFile(matrix[b1, b2]);
+                    curButton.Enabled = false;
+                }
+            }
+            else
+            {
+                triesLabel.Text = "GAME OVER!";
+            }
+        }
+
+        //restart game button
+        private void restartGame(object sender, EventArgs e)
+        {
+            triesLeft = 2;
+            secondPress = false;
+            //enabling buttons
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    string bName = "b" + i.ToString() + j.ToString();
+                    Button btn = this.Controls[bName] as Button;
+                    btn.Enabled = true;
+                }
+            }
+            showImages();
+        }
+
+
+        //show images on buttons for 10 seconds
+        private void showImages()
+        {
+            counter = 10;
+            timer1.Enabled = true;
+            timer1.Start();
+            //randomize
+            Random rnd = new Random();
+            string[] randImages = images.OrderBy(x => rnd.Next()).ToArray();
+
+            //add images to buttons and add them to the matrix also for further checkup
+            int k = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+
+                    string bName = "b" + i.ToString() + j.ToString();
+
+                    Button btn = this.Controls[bName] as Button;
+                    matrix[i, j] = randImages[k];
+                    btn.BackgroundImage = Image.FromFile(randImages[k]);
+                    //disable buttons so no pressing when timer is not 0
+                    btn.Enabled = false;
+                    k++;
+                }
+            }
+        }
+
+        //hide images after timeout
+        private void hideImages()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    string bName = "b" + i.ToString() + j.ToString();
+                    Button btn = this.Controls[bName] as Button;
+                    btn.BackgroundImage = null;
+                    btn.Enabled = true;
+                }
+            }
+        }
+
+        //timer countdown
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeLabel.Text = counter.ToString();
+            counter--;
+            if (counter == 0)
+            {
+                timer1.Stop();
+                hideImages();
+                timeLabel.Text = "";
+            }
+
+        }
+
+    }
+}
